ee2c7cbc8f4a10c4e2566eca9bdb86708ea822ef
Salvatore <salvatore.fiannaca1@gmail.com>
2021-04-17 22:23:26 +0200

    prepare RecyclerView

---
+++ app/src/main/java/com/cashmanagment/adapter/RecyclerAdapter.kt
@@ -0,0 +1,38 @@
+package com.cashmanagment.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.cashmanagment.R
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
+
+    private var amount = arrayOf("100", "20", "50")
+    private var tags = arrayOf("all", "all", "all")
+    private var types = arrayOf("0", "1", "0")
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_history, parent, false)
+        return ViewHolder(v)
+
+    }
+
+    override fun getItemCount(): Int {
+        return amount.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        holder.itemAmount.text = "€${types[position]}"
+        holder.itemTag.text = tags[position]
+    }
+
+    inner class  ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var itemAmount: TextView = itemView.findViewById(R.id.item_amount)
+        var itemTag: TextView = itemView.findViewById(R.id.item_tag)
+
+    }
+
+}
+
--- app/src/main/java/com/cashmanagment/fragments/History.kt
+++ app/src/main/java/com/cashmanagment/fragments/History.kt
@@ -1,27 +1,56 @@
 package com.cashmanagment.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cashmanagment.R
+import com.cashmanagment.adapter.RecyclerAdapter
+import com.cashmanagment.database.DatabaseHandler
 import com.cashmanagment.databinding.FragmentDashboardBinding
 import com.cashmanagment.databinding.FragmentHistoryBinding
+import com.cashmanagment.models.ActionModel
 
 class History : Fragment() {
 
     private lateinit var binding: FragmentHistoryBinding
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FragmentHistoryBinding.inflate(layoutInflater)
+
+        val items = getActions()
+        for (i in items){
+            Log.e("item id:", i.id.toString())
+            Log.e("item tag", i.tag)
+            Log.e("item type", i.type.toString())
+            Log.e("item amount", i.amount.toString())
+
+        }
+
+
+        layoutManager = LinearLayoutManager(this.requireContext())
+        binding.recyclerView.layoutManager = layoutManager
+        adapter = RecyclerAdapter()
+        binding.recyclerView.adapter = adapter
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    private fun getActions(): ArrayList<ActionModel> {
+        val dbHandler = DatabaseHandler(this.requireContext())
+        return dbHandler.readAll()
     }
 
 }
