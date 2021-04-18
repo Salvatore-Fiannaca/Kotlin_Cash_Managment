9f682758872cf2cbc5c2ebc89324481df52afbc2
Salvatore <salvatore.fiannaca1@gmail.com>
2021-04-18 15:26:13 +0200

    refactory layout for History

--- app/src/main/java/com/cashmanagment/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/cashmanagment/adapter/RecyclerAdapter.kt
@@ -11,7 +11,7 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
 
     private var amount = arrayOf("100", "20", "50")
     private var tags = arrayOf("all", "all", "all")
-    private var types = arrayOf("0", "1", "0")
+    private var types = arrayOf("+", "-", "+")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.item_history, parent, false)
@@ -24,13 +24,15 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
     }
 
     override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
-        holder.itemAmount.text = "€${types[position]}"
+        holder.itemAmount.text = amount[position]
+        holder.itemType.text = types[position]
         holder.itemTag.text = tags[position]
     }
 
     inner class  ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         var itemAmount: TextView = itemView.findViewById(R.id.item_amount)
         var itemTag: TextView = itemView.findViewById(R.id.item_tag)
+        var itemType: TextView = itemView.findViewById(R.id.item_type)
 
     }
 
