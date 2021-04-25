869d8b363db40017d64f510634c4dcf06e858c37
Salvatore <salvatore.fiannaca1@gmail.com>
2021-04-25 16:16:22 +0200

    Fix counter under 1kkk

--- app/src/main/java/com/cashmanagment/utils/Utils.kt
+++ app/src/main/java/com/cashmanagment/utils/Utils.kt
@@ -1,15 +1,38 @@
 package com.cashmanagment.utils
 
 import android.util.Log
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 class Utils {
 
     fun cleanDoubleToString(counter: Double): String {
         var txt = "${counter.toFloat()}"
-        val beforeDot = txt.substringBeforeLast(".")
+        var beforeDot = txt.substringBeforeLast(".")
         val afterDot = txt.substringAfterLast(".")
 
-        if (afterDot.toInt() > 0) {
+        if (beforeDot.length >3){
+            var reverseString = beforeDot.reversed()
+            var arrString = ArrayList<String>();
+            var i = 1
+
+            for (c in reverseString) {
+                arrString.add(c.toString())
+
+                val isMultipleOfThree = (i % 3) == 0
+                val isNotLastChar = i < reverseString.length
+
+                if (isMultipleOfThree && isNotLastChar) {
+                    arrString.add(".")
+                }
+                i++
+            }
+
+            val fixedString = arrString.reversed().joinToString("")
+            beforeDot = fixedString
+        }
+
+        if (afterDot.toDouble() > 0) {
             txt = "$beforeDot.$afterDot"
         } else {
             txt = beforeDot
